cdc4437d7f6e09b7bca84c866f912adb06110048
Bubbah69 <Bubbah69@users.noreply.github.com>
2021-11-09 14:32:44 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+function AllStop () {
+    maqueen.motorStop(maqueen.Motors.All)
+    move = ""
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+}
 radio.onReceivedString(function (receivedString) {
     maqueen.motorStop(maqueen.Motors.All)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
@@ -31,23 +44,19 @@ basic.forever(function () {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        move = ""
     }
     if (move == "L") {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        move = ""
     }
     if (move == "B") {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+        move = ""
     }
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 5) {
-        maqueen.motorStop(maqueen.Motors.All)
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 5) {
+        AllStop()
     }
 })
